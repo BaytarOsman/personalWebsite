82fe2af264053b0f7753a78092b093689ef2dd54
BaytarOsman <bosman1997@hotmail.com>
2023-07-25 15:58:31 +0300

    toast nearly done

--- osmanbaytar/src/components/contact/ContactForm.jsx
+++ osmanbaytar/src/components/contact/ContactForm.jsx
@@ -1,10 +1,12 @@
 import { useFormik } from "formik";
 import { contactSchemaEn, contactSchemaTr } from "../../schemas";
 import "../../styles/ContactForm.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import contactFormData from "../../data/contact/contactFormData";
 import { useSelector } from "react-redux";
 import { ContactTitle } from "../../styles/contact/ContactStyle";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const onSubmit = async (values, actions) => {
   await new Promise((resolve) => {
@@ -69,6 +71,108 @@ function ContactForm() {
       onSubmit,
     });
 
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
+
+  function nameWarningToastEn() {
+    toast.warning("Name can not be empty!", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  function nameWarningToastTr() {
+    toast.warning("İsim boş bırakılamaz!", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  function emailWarningToastEn() {
+    toast.warning("Email can not be empty!", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  function emailWarningToastTr() {
+    toast.warning("Email boş bırakılamaz!", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  function messageWarningToastEn() {
+    toast.warning("Message can not be empty!", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  function messageWarningToastTr() {
+    toast.warning("Mesaj boş bırakılamaz!", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+
+  function checkToast() {
+    if (language === "en") {
+      if (values.name === "") {
+        nameWarningToastEn();
+        nameRef.current.focus();
+      } else if (values.email === "") {
+        emailWarningToastEn();
+        emailRef.current.focus();
+      } else if (values.message === "") {
+        messageWarningToastEn();
+        messageRef.current.focus();
+      }
+    } else if (language === "tr") {
+      if (values.name === "") {
+        nameWarningToastTr();
+        nameRef.current.focus();
+      } else if (values.email === "") {
+        emailWarningToastTr();
+        emailRef.current.focus();
+      } else if (values.message === "") {
+        messageWarningToastTr();
+        messageRef.current.focus();
+      }
+    }
+  }
+
   return (
     <form onSubmit={handleSubmit} className="contactForm">
       <ContactTitle>{title}</ContactTitle>
@@ -80,6 +184,7 @@ function ContactForm() {
             value={values.name}
             onChange={handleChange}
             id="name"
+            ref={nameRef}
             placeholder={namePlaceholder}
             className={errors.name ? "input-error" : ""}
           />
@@ -92,6 +197,7 @@ function ContactForm() {
             value={values.email}
             onChange={handleChange}
             id="email"
+            ref={emailRef}
             placeholder={emailPlaceholder}
             className={errors.email ? "input-error" : ""}
           />
@@ -105,6 +211,7 @@ function ContactForm() {
           value={values.message}
           onChange={handleChange}
           id="message"
+          ref={messageRef}
           placeholder={messagePlaceholder}
           className={errors.message ? "input-error" : ""}
         />
@@ -112,10 +219,11 @@ function ContactForm() {
       </div>
 
       <div className="d-flex justify-content-center">
-        <button type="submit" disabled={isSubmitting}>
+        <button type="submit" disabled={isSubmitting} onClick={checkToast}>
           {button}
         </button>
       </div>
+      <ToastContainer />
     </form>
   );
 }
